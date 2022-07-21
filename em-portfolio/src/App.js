13bac170e4e632beac166895eb5515a1820ba490
Majikro <majikro@gmail.com>
2022-07-21 09:42:54 -0400

    upload

--- em-portfolio/src/App.js
+++ em-portfolio/src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-<<<<<<< HEAD
           Ethan's Future Portfolio!
-=======
-          Ethan's Future Portfolio
->>>>>>> 83b2a0e446624aad15107ec44d3e72cb7589f796
         </h1>
         <a
           className="App-link"
