99b34597e1ebcdfc3951f5fb5d9ca9d749e9e374
Magikro <majikrow@gmail.com>
2022-08-31 14:38:41 -0400

    new template

--- em-portfolio/src/App.js
+++ em-portfolio/src/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import Layout from "./pages/Layout";
-import About from "./pages/About";
-import Login from "./pages/Login";
+import logo from './logo.svg';
+import './App.css';
 
-
-
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-        <Layout />
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/login" element={<Login/>}/>
-          <Route path="*" element={<NoPage/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );    
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
